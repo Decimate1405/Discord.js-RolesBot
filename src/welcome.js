4cf7053dc5fd3be67da494a6ce0b096a63241aba
Anirudh Kurma <anirudhk313@gmail.com>
2021-02-05 10:42:49 -0500

    Create welcome.js

---
+++ src/welcome.js
@@ -0,0 +1,14 @@
+const channelID = "798265379494690846";
+const rolesChannel = "800556485209489438";
+
+module.exports = (client) => {
+  client.on("guildMemberAdd", (member) => {
+    console.log(member);
+    const message = `Welcome <@${
+      member.id
+    }>. Select a role in ${member.guild.channels.get(rolesChannel).toString()}`;
+
+    const channel = member.guild.channels.cache.get(channelID);
+    channel.send(message);
+  });
+};
