87aa5c380e0e8a62ed49cd9ce005a2ec460aa803
Anirudh Kurma <anirudhk313@gmail.com>
2021-02-05 11:04:03 -0500

    Update welcome.js

--- src/welcome.js
+++ src/welcome.js
@@ -4,15 +4,15 @@ const client = new Client({
 });
 
 module.exports = (client) => {
+  const channelID = "798265379494690846";
+  const rolesChannel = "800556485209489438";
   client.on("guildMemberAdd", (member) => {
-    const channelID = "798265379494690846";
-    const rolesChannel = "800556485209489438";
     console.log(member);
     const message = `Welcome <@${
       member.id
-    }>. Select a role in ${client.guild.channels.get(rolesChannel).toString()}`;
+    }>. Select a role in ${member.guild.channels.get(rolesChannel).toString()}`;
 
-    const channel = client.guild.channels.cache.get(channelID);
+    const channel = member.guild.channels.cache.get(channelID);
     channel.send(message);
     message.member.send("Hi");
     message.author.send("Lol");
