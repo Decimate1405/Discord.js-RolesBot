ceb8e4554a18951d712d5fd091100511f71ec2e7
Anirudh Kurma <anirudhk313@gmail.com>
2021-02-05 11:24:54 -0500

    Update welcome.js

--- src/welcome.js
+++ src/welcome.js
@@ -12,6 +12,6 @@ module.exports = (client) => {
     }>. Select a role in ${client.channels.cache.get(rolesChannel).toString()}`;
 
     client.channels.cache.get("798265379494690846").send(message);
-    message.author.send("Lol");
+    member.author.send("Lol");
   });
 };
